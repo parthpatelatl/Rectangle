1b6d6296262c8b91bbec62ff356575af4e5910cc
Parth Patel <parthpatelatl@gmail.com>
2018-02-27 10:44:26 -0500

    Update TestRectangle.java
    Added additional comments/spacing

--- TestRectangle.java
+++ TestRectangle.java
@@ -19,10 +19,11 @@ public class TestRectangle {
         rectangles[0] = new Rectangle(4,40);
         rectangles[1] = new Rectangle(3.5, 35.9);
         
+        //Creating a formatting object to show only two places after the decimal
         DecimalFormat numberFormat = new DecimalFormat("#.00");
         
         //For loop goes through and prints out all details of each triangle
-        for (int i=0; i<2;i++){
+        for (int i = 0; i < 2; i++){
             System.out.println("Rectangle " + (i+1) + ":" );
             System.out.println("This rectangle has a width of " + numberFormat.format(rectangles[i].getWidth()) + ", and a height of " + numberFormat.format(rectangles[i].getHeight()) + ".");
             System.out.println("The area of this rectangle is " + numberFormat.format(rectangles[i].getArea()) + ", and the perimeter of the triangle is " + numberFormat.format(rectangles[i].getPerimeter()) + ".");
